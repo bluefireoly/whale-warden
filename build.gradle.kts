4b4b2d87388a670f14ebc9b8e38f29af81da1149
Jakob K <bluefireolymp@gmail.com>
2021-04-02 00:36:02 +0200

    Init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+val jvmVersion = JavaVersion.VERSION_11
+val jvmVersionString = "11"
+
+val ktorVersion = "1.5.2"
+
+val entryClass = "net.axay.whalewarden.ManagerKt"
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+    application
+    id ("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+group = "net.axay"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+}
+
+java.sourceCompatibility = jvmVersion
+java.targetCompatibility = jvmVersion
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = jvmVersionString
+}
+
+application {
+    mainClass.set(entryClass)
+
+    // until https://github.com/johnrengelman/shadow/issues/609 is fixed
+    @Suppress("DEPRECATION")
+    mainClassName = entryClass
+}
