a81b5e651c664f83ddd43f694ff8cc6188c732da
Jakob K <bluefireolymp@gmail.com>
2021-04-02 21:03:36 +0200

    Remove old project structure

--- src/main/kotlin/net/axay/whalewarden/Env.kt
+++
@@ -1,9 +0,0 @@
-package net.axay.whalewarden
-
-object Env {
-    private fun env(name: String): String? = System.getenv(name)
-
-    val webhookToken = env("WEBHOOK_TOKEN")
-    val webhookPath: String = env("WEBHOOK_PATH") ?: "/webhook/update"
-    val containerNames = env("UPDATE_CONTAINERS")?.split(' ') ?: emptyList()
-}
