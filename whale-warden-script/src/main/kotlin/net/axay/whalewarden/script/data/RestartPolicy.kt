0cd69df7f35e50de31a5bbffcbfc6696ba307ad8
Jakob K <bluefireolymp@gmail.com>
2021-04-03 00:04:03 +0200

    Update RestartPolicy.kt

--- whale-warden-script/src/main/kotlin/net/axay/whalewarden/script/data/RestartPolicy.kt
+++ whale-warden-script/src/main/kotlin/net/axay/whalewarden/script/data/RestartPolicy.kt
@@ -5,6 +5,9 @@ class RestartPolicy(
     val maximumRetryCount: Int,
 ) {
     companion object {
+        val NO_RESTART = RestartPolicy("", 0)
         val ALWAYS = RestartPolicy("always", 0)
+        val UNLESS_STOPPED = RestartPolicy("unless-stopped", 0)
+        val ON_FAILURE = RestartPolicy("on-failure", 0)
     }
 }
