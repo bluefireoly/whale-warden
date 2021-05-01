7ec86503cf53c8bdec539afcc04038ff3c795de3
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-01 21:18:15 +0200

    Update build.gradle.kts

--- whale-warden-script/build.gradle.kts
+++ whale-warden-script/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.4.32"
+    `kotlin-build-script`
 }
 
-group = "net.axay"
-version = "0.0.1"
-
-repositories {
-    mavenCentral()
+dependencies {
+    implementation(project(":whale-warden-common"))
 }
