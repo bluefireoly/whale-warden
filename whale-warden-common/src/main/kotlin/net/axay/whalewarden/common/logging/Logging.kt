c68fcda9e2c1a36b2eddac6fb8c4d97693580c4e
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-01 22:40:35 +0200

    Update Logging.kt

--- whale-warden-common/src/main/kotlin/net/axay/whalewarden/common/logging/Logging.kt
+++ whale-warden-common/src/main/kotlin/net/axay/whalewarden/common/logging/Logging.kt
@@ -4,3 +4,13 @@ package net.axay.whalewarden.common.logging
  * Logs an information. The implementation of this function may change.
  */
 fun logInfo(info: String) = println("INFO > $info")
+
+/**
+ * Logs an error. The implementation of this function may change.
+ */
+fun logError(info: String) = System.err.println("ERROR > $info")
+
+/**
+ * Logs this exception without the stacktrace, using [logError].
+ */
+fun Exception.logError() = logError("(${this::class.simpleName}) $message")
