9c224294ec77e0f6f56cb149b905925b6474435f
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-01 18:51:13 +0200

    Add Mount class

--- whale-warden-script/src/main/kotlin/net/axay/whalewarden/script/registry/Registry.kt
+++ whale-warden-script/src/main/kotlin/net/axay/whalewarden/script/registry/Registry.kt
@@ -1,7 +1,11 @@
 package net.axay.whalewarden.script.registry
 
+import net.axay.whalewarden.script.data.Network
 import net.axay.whalewarden.script.data.Service
+import net.axay.whalewarden.script.data.Volume
 
 object Registry {
     val services = mutableListOf<Service>()
+    val networks = mutableListOf<Network>()
+    val volumes = mutableListOf<Volume>()
 }
