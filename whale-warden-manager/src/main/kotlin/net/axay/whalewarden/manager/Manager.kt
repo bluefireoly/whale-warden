be15cfc2f365f34325a4797b2e92ea79c58fe7f4
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-01 21:18:20 +0200

    Update Manager.kt

--- whale-warden-manager/src/main/kotlin/net/axay/whalewarden/manager/Manager.kt
+++ whale-warden-manager/src/main/kotlin/net/axay/whalewarden/manager/Manager.kt
@@ -2,21 +2,20 @@ package net.axay.whalewarden.manager
 
 import com.github.dockerjava.api.DockerClient
 import com.github.dockerjava.api.command.PullImageResultCallback
+import com.github.dockerjava.api.model.PullResponseItem
 import com.github.dockerjava.core.DefaultDockerClientConfig
 import com.github.dockerjava.core.DockerClientImpl
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.netty.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import net.axay.whalewarden.common.logging.logInfo
+import net.axay.whalewarden.manager.script.applyService
+import net.axay.whalewarden.manager.script.runConfigScript
+import net.axay.whalewarden.script.registry.Registry
 
 val dockerClient: DockerClient = run {
-    val config = DefaultDockerClientConfig.createDefaultConfigBuilder().build()
+    val config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+        .withDockerHost("unix:///var/run/docker.sock")
+        .build()
     val httpClient = ApacheDockerHttpClient.Builder()
         .dockerHost(config.dockerHost)
         .build()
@@ -24,48 +23,68 @@ val dockerClient: DockerClient = run {
     DockerClientImpl.getInstance(config, httpClient)
 }
 
-fun main(args: Array<String>) = EngineMain.main(args)
+suspend fun main() = withContext(Dispatchers.IO) {
+    runConfigScript()
 
-fun Application.mainModule() {
-    routing {
-        get(Env.webhookPath) {
-            val token = call.parameters["token"]
-            if (token == Env.webhookToken) {
-                withContext(Dispatchers.IO) {
-                    println("Listing containers")
-                    val updateContainers = dockerClient.listContainersCmd()
-                        .withNameFilter(Env.containerNames)
-                        .exec()
+    val createJobs = mutableListOf<Job>()
+    Registry.services.forEach {
+        createJobs += launch {
+            val pullCallback = PullImageResultCallback()
+            dockerClient.pullImageCmd(it.image).exec(pullCallback)
+            pullCallback.awaitCompletion()
+            logInfo("pulled image ${it.image}")
 
-                    updateContainers
-                        .mapTo(HashSet()) {
-                            println("Image of container ${it.names[0]} is ${it.image}")
-                            it.image
-                        }
-                        .map {
-                            launch {
-                                val callback = PullImageResultCallback()
-                                dockerClient.pullImageCmd(it).exec(callback)
-                                callback.awaitStarted()
-                                println("Now pulling image with name $it...")
-                                callback.awaitCompletion()
-                                println("Pulled image with name $it")
-                            }
-                        }.joinAll()
-
-                    updateContainers.map {
-                        launch {
-                            dockerClient.removeContainerCmd(it.id).exec()
-                            println("Updated container ${it.id} with name ${it.names[0]}")
-                        }
-                    }.joinAll()
-
-                    println("Updated all containers")
-                }
-                call.respond(HttpStatusCode.OK, "Updated containers")
-            } else {
-                call.respond(HttpStatusCode.Unauthorized, "Wrong or missing token")
-            }
+            dockerClient.createContainerCmd(it.image).applyService(it).exec()
+            logInfo("Created service: ${it.name}")
         }
     }
+
+    createJobs.joinAll()
+    logInfo("All services created")
 }
+
+//fun main(args: Array<String>) = EngineMain.main(args)
+//
+//fun Application.mainModule() {
+//    routing {
+//        get(Env.webhookPath) {
+//            val token = call.parameters["token"]
+//            if (token == Env.webhookToken) {
+//                withContext(Dispatchers.IO) {
+//                    println("Listing containers")
+//                    val updateContainers = dockerClient.listContainersCmd()
+//                        .withNameFilter(Env.containerNames)
+//                        .exec()
+//
+//                    updateContainers
+//                        .mapTo(HashSet()) {
+//                            println("Image of container ${it.names[0]} is ${it.image}")
+//                            it.image
+//                        }
+//                        .map {
+//                            launch {
+//                                val callback = PullImageResultCallback()
+//                                dockerClient.pullImageCmd(it).exec(callback)
+//                                callback.awaitStarted()
+//                                println("Now pulling image with name $it...")
+//                                callback.awaitCompletion()
+//                                println("Pulled image with name $it")
+//                            }
+//                        }.joinAll()
+//
+//                    updateContainers.map {
+//                        launch {
+//                            dockerClient.removeContainerCmd(it.id).exec()
+//                            println("Updated container ${it.id} with name ${it.names[0]}")
+//                        }
+//                    }.joinAll()
+//
+//                    println("Updated all containers")
+//                }
+//                call.respond(HttpStatusCode.OK, "Updated containers")
+//            } else {
+//                call.respond(HttpStatusCode.Unauthorized, "Wrong or missing token")
+//            }
+//        }
+//    }
+//}
