12c2c42e620d6d1f5e8328871d00b9482353d34c
Jakob K <52456572+bluefireoly@users.noreply.github.com>
2021-05-01 18:49:30 +0200

    Update ScriptRunner.kt

--- whale-warden-manager/src/main/kotlin/net/axay/whalewarden/manager/script/ScriptRunner.kt
+++ whale-warden-manager/src/main/kotlin/net/axay/whalewarden/manager/script/ScriptRunner.kt
@@ -3,18 +3,16 @@ package net.axay.whalewarden.manager.script
 import net.axay.whalewarden.manager.Env
 import javax.script.ScriptEngineManager
 
-fun main() = runConfigScript()
-
 fun runConfigScript() {
     val scriptSource = buildString {
         appendLine(
             """
                 import net.axay.whalewarden.script.builder.service
-                import net.axay.whalewarden.script.data.RestartPolicy.Companion.ALWAYS
-                import net.axay.whalewarden.script.data.RestartPolicy.Companion.NO_RESTART
-                import net.axay.whalewarden.script.data.RestartPolicy.Companion.ON_FAILURE
-                import net.axay.whalewarden.script.data.RestartPolicy.Companion.UNLESS_STOPPED
-                import net.axay.whalewarden.script.logging.logInfo.*
+                import net.axay.whalewarden.script.builder.network
+                import net.axay.whalewarden.script.builder.bind
+                import net.axay.whalewarden.script.builder.volume
+                import net.axay.whalewarden.script.data.Service.RestartPolicy.Type.*
+                import net.axay.whalewarden.script.logging.logInfo
             """.trimIndent()
         )
         append(Env.configFile.readText())
